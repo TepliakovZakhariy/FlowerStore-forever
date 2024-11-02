b7fb4391fa727ba84d2d2420eaa6027285399124
cheeese <143966422+TepliakovZakhariy@users.noreply.github.com>
2024-11-02 23:32:26 +0200

    implemented

---
+++ src/test/java/ua/edu/ucu/apps/lab8/DecoratorTests.java
@@ -0,0 +1,42 @@
+package ua.edu.ucu.apps.lab8;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import ua.edu.ucu.apps.lab8.model.Flower;
+import ua.edu.ucu.apps.lab8.decorator.*;
+
+class DecoratorTests {
+
+    @Test
+    void testPaperDecorator() {
+        Flower flower = new Flower();
+        flower.setName("Rose");
+        flower.setPrice(10);
+
+        FlowerDecorator paperWrappedFlower = new PaperDecorator(flower);
+        assertEquals(23, paperWrappedFlower.getPrice());
+        assertEquals("Rose wrapped in paper.", paperWrappedFlower.getDescription());
+    }
+
+    @Test
+    void testBasketDecorator() {
+        Flower flower = new Flower();
+        flower.setName("Tulip");
+        flower.setPrice(8);
+
+        FlowerDecorator basketFlower = new BasketDecorator(flower);
+        assertEquals(12, basketFlower.getPrice());
+        assertEquals("Tulip in a basket", basketFlower.getDescription());
+    }
+
+    @Test
+    void testRibbonDecorator() {
+        Flower flower = new Flower();
+        flower.setName("Lily");
+        flower.setPrice(15);
+
+        FlowerDecorator ribbonFlower = new RibbonDecorator(flower);
+        assertEquals(55, ribbonFlower.getPrice());
+        assertEquals("Lily with ribbon.", ribbonFlower.getDescription());
+    }
+}
